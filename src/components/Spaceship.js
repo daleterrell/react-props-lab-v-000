d0431bebb964edfd4c1a6a9df12182ce1cf2726f
Dale Terrell <tragicid@gmail.com>
2019-05-03 23:30:22 +0000

    Automatically backed up by Learn

--- src/components/Spaceship.js
+++ src/components/Spaceship.js
@@ -1,1 +1,15 @@
-// Code The Spaceship Component Here
+const Spaceship = props => (<p>{props.name}</p>);
+
+Spaceship.defaultProps = {
+  name: 'CamperBot'
+  speed: 'slow'
+  has Rockets: false
+  colors: ['black', 'red']
+  
+};
+
+Spaceship.propTypes = {
+  name: PropTypes.string.isRequired
+};
+
+export default Spaceship;
